4c0f37b5bb3d5e62b2037b77d81df04883fc83e5
lokesh keelisetti <keelisettilokesh@gmail.com>
2021-02-16 19:08:12 +0530

    added feature of downloading the captured imnages

--- index.js
+++ index.js
@@ -11,16 +11,42 @@ if(supported){
         video: true,
     };
 
+    
     captureButton.addEventListener('click', () => {
+        let imageContainer =   document.createElement('div');
+        imageContainer.classList.toggle('image');
+        imageContainer.classList.toggle('m-2');
+
+
         // Draw the video frame to the canvas.
         const canvas = document.createElement('canvas');
-        canvas.classList.toggle('image');
         canvas.width = 320;
         canvas.height = 240;
         const context = canvas.getContext('2d');
         context.drawImage(player, 0, 0, canvas.width, canvas.height);
-        //console.log(canvas);
-        capturedImages.appendChild(canvas);
+        imageContainer.appendChild(canvas);
+
+        let buttonContainer = document.createElement('div');
+        buttonContainer.classList.toggle('row');
+
+        let saveButton = document.createElement('a');
+        saveButton.classList.add('btn','btn-primary','ml-auto');
+        saveButton.textContent = 'Save';
+        saveButton.setAttribute('download', 'MyImage.png');
+        saveButton.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+        buttonContainer.appendChild(saveButton);
+
+        let deleteButton = document.createElement('button');
+        deleteButton.classList.add('btn','btn-danger','mr-auto','ml-2');
+        deleteButton.textContent = 'Delete';
+        deleteButton.onclick = function(){
+            capturedImages.removeChild(imageContainer);
+        };
+        buttonContainer.appendChild(deleteButton);
+
+        imageContainer.appendChild(buttonContainer);
+
+        capturedImages.appendChild(imageContainer);
     });
 
 
